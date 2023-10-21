d50415d5d118565453d388c03310bb83205168aa
Richardsantos2K <richarddossantos2022@gmail.com>
2023-10-21 14:17:42 -0300

    Curso POO dio do bootcamp santander 2023

---
+++ src/main/java/org/example/ReprodutorMusica.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Musica {
+    private String titulo;
+    private String artista;
+    private int duracao;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+}
